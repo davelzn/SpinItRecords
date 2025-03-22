c1849d6408ddee53cfb64a060e1918bef45c5f27
DAVIDE <davideliuzzo189@gmail.com>
2025-03-22 18:11:52 +0100

    Aggiunta classi

--- prodotti/models.py
+++ prodotti/models.py
@@ -8,8 +8,25 @@ class Prodotto(models.Model):
     descrizione = models.CharField(max_length = 5000)
     categoria = models.CharField(max_length = 80)
     immagine = models.ImageField()
+    artista = models.CharField(max_length = 100)
+    anno_uscita = models.CharField(max_length = 5)
+    link_streaming = models.CharField(max_length = 5000)
+    etichetta = models.CharField(max_length = 100)
 
 # ForeignKey
 class Preferito(models.Model):
     id_utente = models.ForeignKey(User, on_delete = models.CASCADE)
     id_prodotto = models.ForeignKey(Prodotto, on_delete =models.CASCADE)
+
+
+class Valutazione(models.Model):
+    id_utente = models.ForeignKey(User, on_delete = models.CASCADE)
+    id_prodotto = models.ForeignKey(Prodotto, on_delete =models.CASCADE)
+    punteggio = models.IntegerField()
+    data = models.DateTimeField(auto_now_add=True)
+
+class Commento(models.Model):
+    id_utente = models.ForeignKey(User, on_delete = models.CASCADE)
+    id_prodotto = models.ForeignKey(Prodotto, on_delete =models.CASCADE)
+    testo = models.CharField(max_length=5000) 
+    data = models.DateTimeField(auto_now_add=True)  
