0828ba8144ebd63e79d78a220bdc5c48e774ad48
DAVIDE <davideliuzzo189@gmail.com>
2025-03-26 18:19:44 +0100

    Aggiunta commenit!

--- prodotti/urls.py
+++ prodotti/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
-from .views import home, dettaglio
+from .views import home, dettaglio, recensioni, preferiti
 from django.conf import settings
 from django.conf.urls.static import static 
 
 urlpatterns = [
     path('', home, name = "home"),
     path( '<int:id>/', dettaglio, name= "dettaglio"),
+    path('recensioni/', recensioni, name="recensioni"),
+    path('preferiti/', preferiti, name="preferiti"),
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
--- prodotti/views.py
+++ prodotti/views.py
@@ -26,3 +26,21 @@ def dettaglio(request, id):
                 "prod.etichetta" : prod.etichetta
                 }
         return render(request, "prodotti/dettaglio.html", context)
+def recensioni(request):
+    if request.method=="GET":
+        prod = Prodotto.objects.get(id=id)
+        context = {
+                'prod': prod,
+                "prod.nome": prod.nome,
+                "prod.descrizione": prod.descrizione,
+                "prod.categoria": prod.categoria,
+                "prod.immagine": prod.immagine,
+                "prod.artista" : prod.artista,
+                "prod.anno_uscita" : prod.anno_uscita,
+                "prod.link_streaming" : prod.link_streaming,
+                "prod.etichetta" : prod.etichetta
+                }
+    return render(request, 'prodotti/recensioni.html', context)
+
+def preferiti(request):
+    return render(request, 'prodotti/preferiti.html')
